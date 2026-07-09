3f5897bc749c37adb75d4d55d4e01f37976ecdb2
beymax11 <dimaandalrimvernon@gmail.com>
2026-07-09 16:43:15 +0800

    chore: trigger deployment rebuild with root directory set to web

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Trigger deployment rebuild with root directory set to web
 };
 
 export default nextConfig;
